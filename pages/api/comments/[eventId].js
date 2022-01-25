3cb1f5c9f03a789f4da60c6c5eeb2fe534bf2966
SEIFU MEKONNEN TOLOSA <tms.18@yahoo.com>
2022-01-25 15:07:01 +0000

    Add api routes for comments

---
+++ pages/api/comments/[eventId].js
@@ -0,0 +1,43 @@
+function handler(req, res) {
+  const eventId = req.query.eventId
+
+  if (req.method === 'POST') {
+    const { email, name, text } = req.body
+
+    if (
+      !email ||
+      email.trim() === '' ||
+      !email.includes('@') ||
+      !name ||
+      name.trim() === '' ||
+      !text ||
+      text.trim() === ''
+    ) {
+      res.status(422).json({ message: 'Invalid input' })
+      return
+    }
+
+    const newComment = {
+      id: new Date().toISOString(),
+      email,
+      name,
+      text,
+      eventId,
+    }
+
+    console.log('NewComment', newComment)
+
+    res.status(200).json({ message: 'Added comment', comment: newComment })
+  }
+
+  if (req.method === 'GET') {
+    const dummy_Data = [
+      { id: 'c1', name: 'Max', text: 'this is my first comment' },
+      { id: 'c2', name: 'Manchester', text: 'this is my second comment' },
+    ]
+
+    res.status(200).json({ comments: dummy_Data })
+  }
+}
+
+export default handler
