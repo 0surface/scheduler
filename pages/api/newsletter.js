ca9b4df9f203a95c21b502f384ef66498ab20c15
SEIFU MEKONNEN TOLOSA <tms.18@yahoo.com>
2022-01-31 11:04:28 +0000

    Running mongodb queries

--- pages/api/newsletter.js
+++ pages/api/newsletter.js
@@ -1,10 +1,24 @@
-function handler(req, res) {
+import { MongoClient } from 'mongodb'
+
+const { MONGODB_URI } = process.env
+
+async function handler(req, res) {
   if (req.method === 'POST') {
     const userEmail = req.body.email
     if (!userEmail || !userEmail.includes('@')) {
       res.status(422).json({ message: 'Invalid email adderss' })
       return
     }
+
+    const client = await MongoClient.connect(MONGODB_URI)
+
+    const db = client.db()
+
+    await db.collection('emails').insertOne({
+      email: userEmail,
+    })
+
+    client.close()
 
     res.status(200).json({ message: 'Signed up' })
   }
